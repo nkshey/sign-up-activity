7ffd13b27fc6bcc23170ea225d901578c130e324
nkshey <peiqrishvili777@gmail.com>
2022-11-22 19:17:54 +0400

    first commit

---
+++ app/src/main/java/com/example/signupactivity/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.signupactivity
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var title = findViewById<TextView>(R.id.title_sign_up)
+        var button = findViewById<TextView>(R.id.button)
+
+        button.setOnClickListener {
+            var email = findViewById<EditText>(R.id.input_email).text.toString()
+            var password = findViewById<EditText>(R.id.input_password).text.toString()
+
+            if (email == "" && password == "") {
+                Toast.makeText(this, "Email and Password fields are empty", Toast.LENGTH_SHORT).show()
+            } else if (email == "") {
+                Toast.makeText(this, "Email field is empty", Toast.LENGTH_SHORT).show()
+            } else if (password == "") {
+                Toast.makeText(this, "Password field is empty", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
+                title.setTextColor(Color.parseColor("#00FF40"))
+            }
+        }
+        }
+        }
